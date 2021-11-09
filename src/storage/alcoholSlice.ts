071555619d51bcf1f1e8397997cd58e1c59c0171
Kacper Szarkiewicz <kacperszarkiewicz@MacBook-Pro-Kacper.local>
2021-11-09 14:00:46 +0100

    remove unused imports

--- src/storage/alcoholSlice.ts
+++ src/storage/alcoholSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../app/store';
 import { Alcohol } from '../types';
 import { getUniqueID } from '../utils';
