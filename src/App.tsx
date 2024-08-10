a739782973c8be91477ec4f3b57c7aa6e5374a96
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-11 01:28:48 +0200

    add welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import ChoicePage from './pages/ChoicePage/ChoicePage';
 import PlayersPage from './pages/PlayersPage/PlayersPage';
 import GamePage from './pages/GamePage/GamePage';
 import FinishedPage from './pages/FinishedPage';
+import WelcomePage from './pages/WelcomePage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ChoicePage />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/choice" element={<ChoicePage />} />
         <Route path="/players" element={<PlayersPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/finish" element={<FinishedPage />} />
---
+++ src/pages/WelcomePage/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+// @ts-ignore
+import ConfettiGenerator from 'confetti-js';
+import { NormalLayout } from '../../layouts/NormalLayout';
+import { RouteButton } from '../../styles/elements';
+import IMG from './converted_logo.png';
+// @ts-ignore
+import ThemeSound from './theme.mp3';
+
+const WelcomePage = () => {
+  useEffect(() => {
+    const confettiSettings = { target: 'my-canvas' };
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+
+    return () => confetti.clear();
+  }, []);
+
+  useEffect(() => {
+    const themeSound = new Audio(ThemeSound);
+
+    themeSound.loop = true;
+
+    themeSound.play();
+
+    return () => {
+      themeSound.pause();
+      themeSound.currentTime = 0;
+    };
+  }, []);
+
+  return (
+    <>
+      <canvas id="my-canvas" style={{ position: 'absolute', zIndex: 2 }}></canvas>
+      <div style={{ width: '60%', margin: 'auto' }}>
+        <NormalLayout>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+              marginTop: 30,
+            }}
+          >
+            <img src={IMG} style={{ width: 700, cursor: 'pointer' }} alt="Start playing wakajki" />
+            <RouteButton to="choice" style={{ zIndex: 999 }}>
+              Rozpocznij grę
+            </RouteButton>
+          </div>
+        </NormalLayout>
+      </div>
+    </>
+  );
+};
+
+export default WelcomePage;
