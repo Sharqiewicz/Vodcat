8ef67cf4b2a1e9d7c1ab4e5b5c6577172f9d0f38
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-16 23:18:08 +0200

    update wheel of fortune

--- src/storage/alcoholSlice.ts
+++ src/storage/alcoholSlice.ts
@@ -4,15 +4,12 @@ import { Alcohol } from '../types';
 import { getUniqueID } from '../utils';
 
 const defaultAlcohol: Alcohol[] = [
-  { name: 'Keczup', percentage: 0, color: '#B43F3F', bonus: true, id: '423n5k' },
+  { name: 'Oliwa', percentage: 0, color: '#B43F3F', bonus: true, id: '423n5k' },
   { name: 'Wódka', percentage: 40, color: '#F8EDE3', bonus: false, id: '9b9dma' },
-  { name: 'Dębowe Mocne', percentage: 8, color: '#914F1E', bonus: false, id: 'ueue' },
-  { name: 'Lech', percentage: 5, color: '#399918', bonus: false, id: 'fdiuiu2i34u21u4' },
-  { name: 'Whisky', percentage: 40, color: '#C5705D', bonus: false, id: 'fdfdswws' },
-  { name: 'Tatratea Forest Fruit', percentage: 62, color: '#674188', bonus: false, id: 'eurehdhes' },
-  { name: 'Rum', percentage: 38, color: '#ECFFE6', bonus: false, id: 'erhjsdhe232s' },
-  { name: 'Tequila', percentage: 38, color: '#FFB200', bonus: false, id: 'erhjsdhe232s' },
-  { name: 'Gin', percentage: 38, color: '#FFBBCC', bonus: false, id: 'erhjsdhe232s' },
+  { name: 'Tatratea Peach', percentage: 57, color: '#674188', bonus: false, id: 'eujdjirehdhes' },
+  { name: 'Rum', percentage: 38, color: '#ECFFE6', bonus: false, id: 'erhfwdkfkdjsdhe232s' },
+  { name: 'Tequila', percentage: 38, color: '#FFB200', bonus: false, id: 'erhjsdhedsfijfj232s' },
+  { name: 'Gin', percentage: 38, color: '#FFBBCC', bonus: false, id: '324234234' },
 ];
 
 export interface AlcoholState {
